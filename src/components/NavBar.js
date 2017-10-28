f3b036cf291401a61c3fd3aa6b117db03604c1d7
Jasdeep Aulakh <jasdeepaulakh@Jasdeeps-MacBook-Air.local>
2017-10-28 18:04:16 +0100

    NavBar fixed for Mobile

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,48 +1,57 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem } from 'reactstrap';
 
 export default class NavBar extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            isOpen: false
+        };
+    }
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+
+    close(){
+        if(this.state.isOpen ===true){
+        this.setState({
+            isOpen: false
+        })}
+    }
+
     render() {
         return (
-            <div class="navbar navbar-toggleable-md fixed-top navbar-inverse bg-primary">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="container">
-                    <div class="collapse navbar-collapse" id="navbarResponsive">
-                        <a href="../" class="navbar-brand">Jasdeep Aulakh</a>
-                        <ul class="navbar-nav">
-                            <li class="nav-item">
-                                <NavLink className="nav-link" exact to="/">Home</NavLink>
-                            </li>
-                            <li class="nav-item">
-                                <NavLink className="nav-link" to="/about">About</NavLink>
-                            </li>
-                            <li class="nav-item">
-                                <NavLink className="nav-link" to="/work">Work</NavLink>
-                            </li>
-                            <li class="nav-item">
-                                <NavLink className="nav-link" to="/education">Education</NavLink>
-                            </li>
-                            <li class="nav-item">
-                                <NavLink className="nav-link" to="/skills">Skills</NavLink>
-                            </li>
-                            <li class="nav-item">
-                                <NavLink className="nav-link" to="/references">References</NavLink>
-                            </li>
-                        </ul>
-                        {/*<ul class="nav navbar-nav ml-auto">
-                            <li class="nav-item">
-                                <a class="nav-link" href="http://builtwithbootstrap.com/" target="_blank">Built With Bootstrap</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="https://wrapbootstrap.com/?ref=bsw" target="_blank">WrapBootstrap</a>
-                            </li>
-                        </ul>*/}
+            <div>
+                <Navbar className="navbar navbar-toggleable-md fixed-top navbar-inverse bg-primary">
 
-                    </div>
-                </div>
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="navbar-nav">
+                            <NavItem>
+                                <NavLink onClick={this.close.bind(this)} className="nav-link" exact to="/">Home</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink onClick={this.close.bind(this)} className="nav-link" to="/about">About</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink onClick={this.close.bind(this)} className="nav-link" to="/work">Work</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink onClick={this.close.bind(this)} className="nav-link" to="/education">Education</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink onClick={this.close.bind(this)} className="nav-link" to="/skills">Skills</NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
             </div>
+
         )
     }
 }
